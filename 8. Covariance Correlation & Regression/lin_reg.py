e807c9226615a4e16e72b8a6f1a49ec28a8001c7
prouser <knowpro258@gmail.com>
2022-12-17 17:13:17 +0530

    added old notebooks (6-10), new notebooks~(33)

--- 8. Covariance Correlation & Regression/lin_reg.py
+++ 8. Covariance Correlation & Regression/lin_reg.py
@@ -57,6 +57,70 @@ print(lr.coef_)
 y_pred = lr.predict(X)
 print(r2_score(y, y_pred))
 
+############### Concrete ###################
+concrete = pd.read_csv(r"C:\Training\Academy\Statistics (Python)\Cases\Concrete Strength\Concrete_Data.csv")
+X = concrete.drop('Strength', axis=1)
+y = concrete['Strength']
+lr = LinearRegression()
+lr.fit(X,y)
+print(lr.intercept_)
+print(lr.coef_)
+y_pred = lr.predict(X)
+print(r2_score(y, y_pred))
+
+############ Exp Salaries ################
+exp_sals = pd.read_csv("Exp_Salaries.csv")
+dum_sals = pd.get_dummies(exp_sals, drop_first=True)
+X = dum_sals.drop('Salary', axis=1)
+y = dum_sals['Salary']
+lr = LinearRegression()
+lr.fit(X,y)
+print(lr.intercept_)
+print(lr.coef_)
+y_pred = lr.predict(X)
+print(r2_score(y, y_pred))
+
+# Generating the predictions
+salsToPred = pd.read_csv("SalsToPredict.csv")
+dum_pred = pd.get_dummies(salsToPred, drop_first=True)
+predictions = lr.predict(dum_pred)
+
+############ Wedding
+import os
+os.chdir(r"C:\Training\Academy\Business Analytics 3e Resources\Excel Datasets\eba3e_datasets_xls")
+wedding = pd.read_excel("Weddings.xlsx",usecols="A:F",skiprows=2)
+
+# 1.
+X = wedding[['Wedding cost']]
+y = wedding['Attendance']
+lr = LinearRegression()
+lr.fit(X,y)
+print(lr.intercept_)
+print(lr.coef_)
+y_pred = lr.predict(X)
+print(r2_score(y, y_pred))
+
+# 2.
+X = wedding[['Wedding cost']]
+y = wedding['Value Rating']
+lr = LinearRegression()
+lr.fit(X,y)
+print(lr.intercept_)
+print(lr.coef_)
+y_pred = lr.predict(X)
+print(r2_score(y, y_pred))
+
+# 3.
+X = wedding[["Couple's Income","Payor"]]
+X = pd.get_dummies(X, drop_first=True)
+y = wedding['Value Rating']
+lr = LinearRegression()
+lr.fit(X,y)
+print(lr.intercept_)
+print(lr.coef_)
+y_pred = lr.predict(X)
+print(r2_score(y, y_pred))
+
 
 
 
